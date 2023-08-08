6a35396824e2e23259de290dbb6ffe97c80c94a6
Kenta Tomita <83906908+Tomiken0503@users.noreply.github.com>
2023-08-08 23:35:36 +0900

    【課題】条件分岐を使用して倍数を表示しよう

---
+++ kadai_007/calc.js
@@ -0,0 +1,20 @@
+const textBox = document.querySelector('input');
+
+textBox.addEventListener('change', () => {
+  const inputNum = textBox.value;
+  if (inputNum !== '') {
+    numCheck(inputNum);
+  }
+});
+
+const numCheck = (num) => {
+  if (num % 3 === 0 && num % 5 === 0) {
+    console.log('3と5の倍数です');
+  } else if (num % 3 === 0) {
+    console.log('3の倍数です');
+  } else if (num % 5 === 0) {
+    console.log('5の倍数です');
+  } else {
+    console.log(num);
+  }
+};
